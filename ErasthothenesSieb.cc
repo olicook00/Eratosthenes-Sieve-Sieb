d7a9bb689c8b99e914e1f0b672408a72ca0873c2
Oliver Cook <149967209+olicook00@users.noreply.github.com>
2023-12-07 17:03:56 +0100

    Update ErasthothenesSieb.cc

--- ErasthothenesSieb.cc
+++ ErasthothenesSieb.cc
@@ -7,10 +7,11 @@ int counter = 0;
 bool prim[n + 1];
 
 int main() {
+    // all values from 1 to n are initially assigned 'true' as primes'
     for (int i = 2; i <= n; i = i + 1) {
         prim[i] = true;
     }
-
+// If they can be divided be a number smaller than their square root, they are not prime and assigned false
     for (int i = 2; i <= n; i = i + 1) {
         for (int j = 2; j <= sqrt(i); j = j + 1) { 
             if (i % j == 0) {
@@ -18,6 +19,7 @@ int main() {
             }
         }
     }
+    // number of remaining 'true' s is then counted to find the number of primes
     for (int k = 2; k <= n; k = k + 1) {
         if (prim[k] == true){
             counter = counter + 1;
